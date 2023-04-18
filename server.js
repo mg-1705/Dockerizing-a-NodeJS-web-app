a306cf172e4a0f1bed843cb312b8e93d3b0b737b
mg-1705 <84842366+mg-1705@users.noreply.github.com>
2023-04-18 12:31:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello my name is Anju Gupta\n');
+  res.send('Hello Excuse me\n');
 });
 
 app.listen(PORT, HOST);
